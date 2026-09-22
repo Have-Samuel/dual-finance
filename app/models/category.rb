4a8c3aa37278247d07defc9e4cdceed6bca1707a
Have Samuel <samiehave9@gmail.com>
2026-09-22 16:12:48 +0300

    Adding the inter- relationship, Validations and some calculations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ApplicationRecord
   belongs_to :user
+
+  has many :transactions, dependent: :restrict_with_error
+  has_many :budgets, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: { scope: :user_id }
 end

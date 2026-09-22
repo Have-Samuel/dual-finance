25370b19c51b96a8f1c980669fab1c8ba0d686f6
Have Samuel <samiehave9@gmail.com>
2026-09-22 16:11:09 +0300

    Adding the inter-relationship and calcutation methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,32 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :transactions, dependent: :destroy
+  has_many :budgets, dependent: :destroy
+  has_many :pots, dependent: :destroy
+  has_many :recurring_bills, dependent: :destroy
+  has_many :categories, dependent: :destroy
+
+  validates :name, presence: true
+
+  def balance_cents
+    transactions.income.sum(:amount_cents) - transactions.expense.sum(:amount_cents)
+  end
+
+  def monthly_income_cents(date = Date.current)
+    transactions.income.where(occurred_on: date.all_month).sum(:amount_cents)
+  end
+
+  def monthly_expense_cents(date = Date.current)
+    transactions.expense.where(occurred_on: date.all_month).sum(:amount_cents)
+  end
+
+  def total_saved_in_pots_cents
+    PotTransaction.joins(:pot).where(pots: { user_id: id }).sum(:amount_cents)
+  end
+
+  def available_balance_cents
+    balance_cents - total_saved_in_pots_cents
+  end
 end
